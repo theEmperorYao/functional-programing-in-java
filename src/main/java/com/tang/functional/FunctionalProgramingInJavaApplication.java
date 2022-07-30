553ae8c23f0bcd23c35a3e3614c969279ebbe05d
tangyao <1013654193@qq.com>
2022-07-30 22:39:17 +0800

    first commit

---
+++ src/main/java/com/tang/functional/FunctionalProgramingInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tang.functional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalProgramingInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalProgramingInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tang/functional/FunctionalProgramingInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgramingInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
